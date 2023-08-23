0bf1fb6de6c78ff74f0312317a052831a050afb7
karthik9 <31131802+karthik984@users.noreply.github.com>
2023-08-22 22:15:48 -0400

    Update Program.cs
    get the author favorite noun in the epub

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using VersOne.Epub;
+using Stanford.NLP;
+using Stanford.NLP.POSTagger;
 
 namespace EPUBWordCounter
 {
@@ -12,55 +14,69 @@ namespace EPUBWordCounter
         {
             string epubFilePath = @"C:\Karthik\zaz\BookAutopsy\source\The Idiot.epub";
 
+            // Load the POS tagger model
+            var posTagger = new EnglishPOSTagger();
+
             EpubBook epubBook = EpubReader.ReadBook(epubFilePath);
 
             List<string> allWords = new List<string>();
-
             foreach (EpubLocalTextContentFile textContentFile in epubBook.ReadingOrder)
             {
                 string content = textContentFile.Content;
-                string[] words = content.Split(new char[] { ' ', '\t', '\n', '\r', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] words = content.Split(new char[] { ' ', '\t', '\n', '\r', '.', ',', ';', '!', '?', '-' }, StringSplitOptions.RemoveEmptyEntries);
                 allWords.AddRange(words);
             }
+
+            // Load a list of common stop words (you can extend this list if needed)
+            var stopWords = new HashSet<string>
+            {
+                "the", "and", "of", "a", "in", "to", "is", "it", "for", "with", "on", "at", "by", "that", "you"
+            };
 
             // Count word occurrences
             Dictionary<string, int> wordCount = new Dictionary<string, int>();
             foreach (string word in allWords)
             {
                 string cleanedWord = word.ToLower(); // Convert to lowercase for consistent counting
-                if (wordCount.ContainsKey(cleanedWord))
+                if (!stopWords.Contains(cleanedWord))
                 {
-                    wordCount[cleanedWord]++;
+                    if (wordCount.ContainsKey(cleanedWord))
+                    {
+                        wordCount[cleanedWord]++;
+                    }
+                    else
+                    {
+                        wordCount[cleanedWord] = 1;
+                    }
                 }
-                else
+            }
+
+            // Get the nouns
+            var nouns = new List<string>();
+            foreach (var word in wordCount.Keys)
+            {
+                var tags = posTagger.Tag(new List<string> { word });
+                if (tags[0] == "NN") // "NN" is the tag for nouns
                 {
-                    wordCount[cleanedWord] = 1;
+                    nouns.Add(word);
                 }
             }
 
-            // Get top 10 words
-            var topWords = wordCount.OrderByDescending(pair => pair.Value)
-                                   .Take(10)
-                                   .Select(pair => pair.Key);
+            // Get top 10 most used nouns
+            var topNouns = nouns.GroupBy(noun => noun)
+                                .OrderByDescending(group => group.Count())
+                                .Take(10)
+                                .Select(group => group.Key);
 
-            // Display top words
-            Console.WriteLine("Top 10 most used words:");
-            foreach (string word in topWords)
+            // Display top nouns
+            Console.WriteLine("Top 10 most used nouns:");
+            foreach (string noun in topNouns)
             {
-                Console.WriteLine($"{word}: {wordCount[word]} occurrences");
+                Console.WriteLine($"{noun}: {wordCount[noun]} occurrences");
             }
 
-                        // Get the top 10 longest words
-            var longestWords = allWords.OrderByDescending(word => word.Length)
-                                      .Take(10);
-
-            // Display top longest words
-            Console.WriteLine("Top 10 longest words:");
-            foreach (string word in longestWords)
-            {
-                Console.WriteLine($"{word}: Length {word.Length}");
-            }
-
+            // Close the EpubBook instance
+            epubBook.Close();
         }
     }
 }
